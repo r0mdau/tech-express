c0e6b3be48f17f16a98b6e837f3c573e054f2c09
Christophe Jollivet <christophe.jollivet@gmail.com>
2014-10-05 22:20:58 +0200

    added middleware for POST on /techs

--- routes/techs.js
+++ routes/techs.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var techs = [{name:'JavaScript', votes:3}, {name:'Java', votes:2}];
+
 router.get('/', function(req, res, next){
-    res.json([{name:'JavaScript', votes:3}, {name:'Java', votes:2}]);
+    res.json(techs);
+});
+
+router.post('/', function(req, res, next){
+    techs.push(req.body);
+    res.status(201).json(req.body);
 });
 
 module.exports = router;
