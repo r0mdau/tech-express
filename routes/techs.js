68449ae09de0caadde49888c4649d19bc25338e3
Christophe Jollivet <christophe.jollivet@gmail.com>
2014-10-05 22:30:06 +0200

    added vote management

--- routes/techs.js
+++ routes/techs.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var techs = [{name:'JavaScript', votes:3}, {name:'Java', votes:2}];
+var techs = [{id: 1, name:'JavaScript', votes:3}, {id: 2,name:'Java', votes:2}];
 
 router.get('/', function(req, res, next){
     res.json(techs);
@@ -10,6 +10,27 @@ router.get('/', function(req, res, next){
 router.post('/', function(req, res, next){
     techs.push(req.body);
     res.status(201).json(req.body);
+});
+
+router.param('techId', function(req, res, next, techId){
+    console.log('param '+ techId);
+    var tech = null;
+    for (var i = techs.length - 1; i >= 0; i--) {
+        if(techs[i].id == techId){
+            req.tech = techs[i];
+            break;
+        };
+    };
+    return next();
+});
+
+router.post('/:techId/vote', function(req, res, next){
+    if(req.tech === null){
+        console.log('not found');
+        res.status(404).json({message:'not found'});
+    }
+    req.tech.votes = req.tech.votes +1;
+    res.json(req.tech);
 });
 
 module.exports = router;
