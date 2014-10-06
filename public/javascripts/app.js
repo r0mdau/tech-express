dde605a2356b1dca35c84471d6d3a41b1d46f97c
Christophe Jollivet <christophe.jollivet@gmail.com>
2014-10-06 06:52:49 +0200

    added form and call for a new Tech

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,12 @@
 angular.module('TechApp',['ngResource'])
     .controller('MainCtrl', ['$scope', 'Techs',function($scope, Techs){
         $scope.techs=Techs.query();
+        $scope.addTech = function(){
+		    var newTech = new Techs({name:$scope.newTechName});
+		    newTech.$save();
+		    $scope.techs.push(newTech);
+		    $scope.newTechName='';
+		}
     }])
     .factory('Techs', ['$resource', function($resource){
         return $resource('/techs/');
